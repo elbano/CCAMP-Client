77140964e2af942ee161ba2a631ab941e099dc44
elbano <elbano28@gmail.com>
2018-10-29 12:59:20 -0400

    update - temporary fix for cardlist

--- src/app/market/creator-discovery/creator-discovery.component.ts
+++ src/app/market/creator-discovery/creator-discovery.component.ts
@@ -9,6 +9,7 @@ import * as MarketActions from '../store/market.actions';
 import { SubscriptionLike } from 'rxjs';
 import { getChannelList } from '../../app.reducer';
 import { GetChannelList } from '../store/market.actions';
+import { Channel } from 'src/app/models/channel.class';
 
 @Component({
   selector: 'app-creator-discovery',
@@ -19,6 +20,7 @@ export class CreatorDiscoveryComponent implements OnInit {
 
   dataSource: CreatorDiscoveryDataSource;
   channelListSubscription: SubscriptionLike;
+  channelList: Channel[];
 
   displayedColumns = ['thumbnail', 'name', 'creationdate'];
 
@@ -26,11 +28,23 @@ export class CreatorDiscoveryComponent implements OnInit {
 
   constructor(private rootStore: Store<fromRoot.State>, private creatorDiscoveryService: CreatorDiscoveryService) { }
 
-  ngOnInit() {
+  ngOnInit() { 
 
     this.rootStore.dispatch(new SpinnerActions.SetSpinnerLoadingState(true));
-    this.dataSource = new CreatorDiscoveryDataSource(this.creatorDiscoveryService, this.rootStore);
-    this.dataSource.fetchChannels();
+    // this.dataSource = new CreatorDiscoveryDataSource(this.creatorDiscoveryService, this.rootStore);
+    // this.dataSource.fetchChannels();
+
+    // Make the call to the ngrx store to fetch the channel list against the api
+    this.rootStore.dispatch(new MarketActions.GetChannelList());
+
+    // We retrieve the channel list by subscribing and setting the value to local variable channelList
+    this.channelListSubscription = this.rootStore.pipe(select(getChannelList)).subscribe(channelListResponse => {
+      if (channelListResponse) {
+         this.channelList = channelListResponse;
+      }
+   });
+
+    
   }
 
 }
